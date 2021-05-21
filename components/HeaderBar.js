d5a3f215e2956996bf9251ed5d98b0577719b2be
Gabriel Brum <gabriel.brum@anestech.com.br>
2021-05-21 14:11:37 -0300

    first

--- components/HeaderBar.js
+++ components/HeaderBar.js
@@ -1,10 +1,10 @@
-import React from 'React';
-import { Appbar } from 'react-native-paper';
+import React from "react";
+import { Appbar } from "react-native-paper";
 
 const HeaderBar = ({ navigation, previous }) => (
   <Appbar.Header>
     {previous && <Appbar.BackAction onPress={navigation.goBack} />}
-    <Appbar.Content title='Imagigram' />
+    <Appbar.Content title="Imagigram" />
   </Appbar.Header>
 );
 
